377f128c1fc546a782ab68b92fe8e9b46e7c0605
Shherardd <Gerardwjones@gmail.com>
2020-09-21 01:35:24 -0500

    @Repository stereotype implementation

--- src/main/java/com/shd/market/persistence/ProductoRepository.java
+++ src/main/java/com/shd/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.shd.market.persistence;
 
 import com.shd.market.persistence.crud.ProductoCrudRepository;
 import com.shd.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository // Or @Component <-- es una generalizacion de un componente de Spring, pero Repository es mas especifico
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -17,7 +19,19 @@ public class ProductoRepository {
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
     }
 
-    public Optional<List<Producto>> getByStock(int cantidadStock, boolean estado){
+    public Optional<List<Producto>> getEscasos(int cantidadStock, boolean estado){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, estado);
+    }
+
+    public Optional<Producto> getProducto (int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void deleteProducto(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
