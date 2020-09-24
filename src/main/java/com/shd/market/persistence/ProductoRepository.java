3494195f41ac440022fac25c6c81073e41ff7f51
Shherardd <Gerardwjones@gmail.com>
2020-09-23 23:56:18 -0500

    repository oriented to domain

--- src/main/java/com/shd/market/persistence/ProductoRepository.java
+++ src/main/java/com/shd/market/persistence/ProductoRepository.java
@@ -1,22 +1,30 @@
 package com.shd.market.persistence;
 
+import com.shd.market.domain.Product;
+import com.shd.market.domain.repository.ProductRepository;
 import com.shd.market.persistence.crud.ProductoCrudRepository;
 import com.shd.market.persistence.entity.Producto;
+import com.shd.market.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository // Or @Component <-- es una generalizacion de un componente de Spring, pero Repository es mas especifico
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
 
-    public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRepository.findAll();
+    @Override
+    public List<Product> getAll() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
 
-    public List<Producto> getByCategoria(int idCategoria) {
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId){
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
     }
 
     public Optional<List<Producto>> getEscasos(int cantidadStock, boolean estado){
