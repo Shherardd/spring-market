a467bbdf7c62522244064e510f96e3be1ee2be82
Shherardd <Gerardwjones@gmail.com>
2020-10-01 01:54:52 -0500

    jwt implementarion and DTOs (request/response)

--- src/main/java/com/shd/market/web/security/JWTUtil.java
+++ src/main/java/com/shd/market/web/security/JWTUtil.java
@@ -1,4 +1,16 @@
 package com.shd.market.web.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
 public class JWTUtil {
+    private static final String KEY = "h4rd";
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
 }
